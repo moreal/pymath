35b028306c2896e030bb27bd99fdc358611e52f7
moreal <dev.moreal@gmail.com>
2019-01-13 00:11:23 +0900

    Add __add__, __mul__ option

--- pymath/__init__.py
+++ pymath/__init__.py
@@ -1,3 +1,6 @@
+class NotFunctionOpertiaonException(Exception):
+    pass
+
 class Function:
 
     def __init__(self, function):
@@ -5,3 +8,21 @@ class Function:
 
     def __call__(self, *args, **kwargs):
         self.function(*args, **kwargs)
+
+    def __add__(self, other: Function):
+        if not isinstance(other, Function):
+            raise NotFunctionOpertiaonException()
+
+        def new_function(x):
+            return self.function(x) + other.function(x)
+
+        return Function(new_function)
+
+    def __mul__(self, other: Function):
+        if not isinstance(other, Function):
+            raise NotFunctionOpertiaonException()
+
+        def new_function(x):
+            return self.function(other.function(x))
+
+        return Function(new_function)
